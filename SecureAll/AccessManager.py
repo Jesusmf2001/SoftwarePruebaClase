b23441c272d4aac2c2b099dd9319b6b20e6162bc
jesus <100429124@alumnos.uc3m.es>
2021-02-26 13:58:30 +0100

    Algoritmo NIF añadido

--- SecureAll/AccessManager.py
+++ SecureAll/AccessManager.py
@@ -7,9 +7,16 @@ class AccessManager:
         pass
 
     def ValidateDNI(self, DNI):
-        # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE DNI
-        # RETURN TRUE IF THE DNI IS RIGHT, OR FALSE IN OTHER CASE
-        return True
+        dict = {0: 'T', 1: 'R', 2: 'W', 3: 'A', 4: 'G', 5: 'M', 6: 'Y', 7: 'F', 8: 'P', 9: 'D', 10: 'X', 11: 'B',
+                12: 'N', 13: 'J', 14: 'Z', 15: 'S', 16: 'Q', 17: 'V', 18: 'H', 19: 'L', 20: 'C', 21: 'K', 22: 'E'}
+        nums = int(NIF[:8])
+        let = NIF[8]
+        res = nums % 23
+        print(nums, let, res)
+        if let == dict[res]:
+            return True
+        else:
+            return False
 
     def ReadaccessrequestfromJSON(self, fi):
 
