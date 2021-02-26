eb76a59d6ee384435c08097182f5aebf09f12468
jesus <100429124@alumnos.uc3m.es>
2021-02-26 14:11:11 +0100

    Algoritmo NIF añadido Modificación

--- SecureAll/AccessManager.py
+++ SecureAll/AccessManager.py
@@ -7,12 +7,12 @@ class AccessManager:
         pass
 
     def ValidateDNI(self, DNI):
+        if len(DNI)==9
         dict = {0: 'T', 1: 'R', 2: 'W', 3: 'A', 4: 'G', 5: 'M', 6: 'Y', 7: 'F', 8: 'P', 9: 'D', 10: 'X', 11: 'B',
                 12: 'N', 13: 'J', 14: 'Z', 15: 'S', 16: 'Q', 17: 'V', 18: 'H', 19: 'L', 20: 'C', 21: 'K', 22: 'E'}
         nums = int(DNI[:8])
         let = DNI[8]
         res = nums % 23
-        print(nums, let, res)
         if let == dict[res]:
             return True
         else:
